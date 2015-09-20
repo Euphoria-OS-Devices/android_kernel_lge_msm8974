60d33b4bbc959cf4e6775eccf7721392719865ac
Tyler Dunn <robotdude@att.net>
2015-09-20 14:30:55 -0400

    cpufreq: Impulse: Bring up to date with interactive tweaks
    cpufreq: interactive: prevents the frequency to directly raise above the
    hispeed_freq from a lower frequency.
    
    When the load was below go_hispeed_load, there is a possibility that
    choose_freq() would return a frequency which would be higher than the
    hispeed_freq. According to the policy we should first jump to the
    hispeed_freq, stay there for above_hispeed_delay and then be allowed to
    raise higher than that.
    
    Added a check to prevent the frequency to be directly raised to
    something higher than the hispeed_freq.
    
    Change-Id: Icda5d848dd9beadcc18835082ddf269732c75bd0
    Signed-off-by: Ruchi Kandoi <kandoiruchi@google.com>
    
    cpufreq: impulse: Do not reschedule timer if policy->max changes
    
    Current implementation rearms timer in CPUFREQ_GOV_LIMITS when
    policy->max increases. That's because CPUs' timers are not rearmed if
    previous frequency evaluation selects policy->max.
    
    With commit 16b90977d0941f2cf623ea58f6bd01ff82c83b25
    ("cpufreq: interactive: Rearm governor timer at max freq"),
    interactive governor timer is always rearmed, and thus timer will
    always fire if CPU is busy. There is no need to rearm timer when
    policy->max changes.
    
    Remove code related to rearming timer for policy->max change in
    CPUFREQ_GOV_LIMITS.
    
    Change-Id: I9c2eb788ab3b1c4cf02fff653cb8919d52752cda
    Signed-off-by: Junjie Wu <junjiew@codeaurora.org>

--- drivers/cpufreq/cpufreq_impulse.c
+++ drivers/cpufreq/cpufreq_impulse.c
@@ -42,7 +42,6 @@ struct cpufreq_impulse_cpuinfo {
 	spinlock_t target_freq_lock; /*protects target freq */
 	unsigned int target_freq;
 	unsigned int floor_freq;
-	unsigned int max_freq;
 	unsigned int min_freq;
 	u64 floor_validate_time;
 	u64 hispeed_validate_time; /* cluster hispeed_validate_time */
@@ -415,6 +414,9 @@ static void cpufreq_impulse_timer(unsigned long data)
 		new_freq = pcpu->policy->cpuinfo.min_freq;
 	} else {
 		new_freq = choose_freq(pcpu, loadadjfreq);
+		if (new_freq > this_hispeed_freq &&
+				pcpu->target_freq < this_hispeed_freq)
+			new_freq = this_hispeed_freq;
 	}
 
 	if (boosted)
@@ -1145,7 +1147,6 @@ static int cpufreq_governor_impulse(struct cpufreq_policy *policy,
 			pcpu->hispeed_validate_time =
 				pcpu->floor_validate_time;
 			pcpu->local_hvtime = pcpu->floor_validate_time;
-			pcpu->max_freq = policy->max;
 			pcpu->min_freq = policy->min;
 			down_write(&pcpu->enable_sem);
 			del_timer_sync(&pcpu->cpu_timer);
@@ -1236,19 +1237,7 @@ static int cpufreq_governor_impulse(struct cpufreq_policy *policy,
 
 			up_read(&pcpu->enable_sem);
 
-			/* Reschedule timer.
-			 * Delete the timers, else the timer callback may
-			 * return without re-arm the timer when failed
-			 * acquire the semaphore. This race may cause timer
-			 * stopped unexpectedly.
-			 */
-			if (policy->max > pcpu->max_freq) {
-				down_write(&pcpu->enable_sem);
-				del_timer_sync(&pcpu->cpu_timer);
-				del_timer_sync(&pcpu->cpu_slack_timer);
-				cpufreq_impulse_timer_resched(j, false);
-				up_write(&pcpu->enable_sem);
-			} else if (anyboost) {
+			if (anyboost) {
 				u64 now = ktime_to_us(ktime_get());
 
 				cpumask_set_cpu(j, &speedchange_cpumask);
@@ -1256,8 +1245,6 @@ static int cpufreq_governor_impulse(struct cpufreq_policy *policy,
 				pcpu->floor_freq = policy->min;
 				pcpu->floor_validate_time = now;
 			}
-
-			pcpu->max_freq = policy->max;
 		}
 		if (anyboost)
 			wake_up_process(speedchange_task);
