0e9c6a468c038730af9ef4f7246e0fbc19af072f
Tyler Dunn <robotdude@att.net>
2015-10-27 17:27:05 -0400

    state_notifier: Change mdss hook, clean and add delay
    Thanks @dorimanx and @neobuddy89!
    
    -Cleaned FB notifiers (We don't use them so remove them and their
    checks)
    -Add 50ms delay to state_suspended to stop races from occuring
    -Changed MDSS hooks to match dori's
    
    Maybe no more SoDs? :)

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -1394,8 +1394,7 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 		if (ctrl_pdata->on_cmds.link_state == DSI_HS_MODE)
 			rc = mdss_dsi_unblank(pdata);
 #ifdef CONFIG_STATE_NOTIFIER
-		if (!use_fb_notifier)
-			state_notifier_call_chain(STATE_NOTIFIER_ACTIVE, NULL);
+		state_resume();
 #endif
 		break;
 	case MDSS_EVENT_BLANK:
@@ -1408,8 +1407,7 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 			rc = mdss_dsi_blank(pdata);
 		rc = mdss_dsi_off(pdata);
 #ifdef CONFIG_STATE_NOTIFIER
-		if (!use_fb_notifier)
-			state_notifier_call_chain(STATE_NOTIFIER_SUSPEND, NULL);
+		state_suspend();
 #endif
 #if defined(CONFIG_LGE_MIPI_DZNY_JDI_INCELL_FHD_VIDEO_PANEL)
         if(touch_driver_registered){
